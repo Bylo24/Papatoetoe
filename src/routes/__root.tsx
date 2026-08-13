36a32f06607e54e209eaa2307d38f047063d56fd
bylo24 <bylobtw@gmail.com>
2026-08-13 22:41:15 +1200

    Remove favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -93,7 +93,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         { name: "twitter:card", content: "summary_large_image" },
       ],
 
-      links: [
+        links: [
         {
           rel: "stylesheet",
           href: appCss,
@@ -108,7 +108,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@600;700&family=Barlow:wght@400;500;600;700&display=swap",
         },
-        { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
       ],
     }),
 
